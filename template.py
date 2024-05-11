5e201b76eea0c2063669c1c3996ed9c77829baad
nagarajRPoojari <np137270@gmail.com>
2024-05-11 14:09:07 +0530

    creating template

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import os
 logging.basicConfig(level=logging.INFO  , format='[%(asctime)s]: %(message)s ')
 
 
-project_name='textSummarizer'
+project_name='LegalAI'
 
 list_of_files=[
     '.github/workflows/.gitkeep',
